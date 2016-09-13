8a77ca173cedebda153cfc41a16aac6730c0db25
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-13 13:54:42 -0600

    add nokogiri to find suggested listings

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/auth/facebook',          as: :facebook_login
   get '/auth/facebook/callback', to: 'sessions#create'
   get '/auth/failure',           to: redirect('/') 
-  delete 'logout',                to: 'sessions#destroy'
+  delete 'logout',               to: 'sessions#destroy'
 end
