a08b7044ca0e047201f36edeb5e36228c43c818d
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-16 14:13:36 -0600

    style to the search listing page

--- app/assets/javascripts/save_listings.js
+++ app/assets/javascripts/save_listings.js
@@ -5,11 +5,17 @@ $(document).ready(function() {
 
 function saveListingButton(){
   $("button[name=button-save]").on("click", function(){
+    // var $this = $(this);
+    // $this.toggleClass('marked');
+    // if($this.hasClass('.marked')){
+    //   $this.text('Saved');
+    // } else {
+    //   $this.text('Save');
+    // }
     var data = ($(this).prop("value"));
     var listingParams = {
           listing: data
         }
-    $.post("/api/v1/user_listings", listingParams) 
+    $.post("/api/v1/user_listings", listingParams);
   })
 }
-
