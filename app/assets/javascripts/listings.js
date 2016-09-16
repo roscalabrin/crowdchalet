48c4bc080e5df49ac420ff2a3200d7b218d632ab
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-16 10:44:47 -0600

    remove js from user dashboard

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
-  saveListing();
-  saveListingButton();
-})
-
-function saveListingButton(){
-  $("button[name=button-save]").on("click", saveListing);
-  // $("button[name=button-save]").on("click", function(){
-  //   var $this = $(this);
-  //     $this.toggleClass('saved');
-  //     if($this.hasClass('saved')){
-  //         $this.text('Saved');         
-  //     } else {
-  //         $this.text('Save');
-  //     }
-  // })
-}
-
-function saveListing(){
-  // $(".save-btn").on("click", function(){
-    var listingParams = {
-      listing: {
-        name: $(this).closest(".listing-name").val(),
-        link: $(this).closest(".listing-link").val()
-      }
-    }
-    console.log($this);
-  // $.post("/api/v1/user_listings", listingParams) 
-}
-// .then(changeButton)
-// function changeButton(){  
+// $(document).ready(function() {
+//   saveListing();
+//   saveListingButton();
+// })
+// 
+// function saveListingButton(){
+//   $("button[name=button-save]").on("click", saveListing);
+//   // $("button[name=button-save]").on("click", function(){
+//   //   var $this = $(this);
+//   //     $this.toggleClass('saved');
+//   //     if($this.hasClass('saved')){
+//   //         $this.text('Saved');         
+//   //     } else {
+//   //         $this.text('Save');
+//   //     }
+//   // })
 // }
 // 
-// function createUserListing() {
-// 
+// function saveListing(){
+//   // $(".save-btn").on("click", function(){
+//     var listingParams = {
+//       listing: {
+//         name: $(this).closest(".listing-name").val(),
+//         link: $(this).closest(".listing-link").val()
+//       }
+//     }
+//     console.log($this);
+//   // $.post("/api/v1/user_listings", listingParams) 
 // }
+// // .then(changeButton)
+// // function changeButton(){  
+// // }
+// // 
+// // function createUserListing() {
+// // 
+// // }
 
 
