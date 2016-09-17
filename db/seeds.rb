fcd6343938eaf1af2e577926799d7f97c20d3a54
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-17 12:11:12 -0600

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start
+    create_users
+    create_searching_groups
+    create_users_groups
+  end
+  
+  def self.create_users
+    100.times do |i|
+      User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        image: Faker::Placeholdit.image("50x50")
+      )
+    end  
+  end
+  
+  def self.create_searching_groups
+    12.times do |i|
+      group_leader = User.offset(rand(User.count)).first.id
+      budgets = [200, 250, 300, 350, 400, 450, 500, 550, 600]
+      locations = ["Vail, CO", "Frisco, CO", "Breckenridge, CO"]
+      SearchingGroup.create(
+        name: Faker::Name.name,
+        description: Faker::Lorem.paragraph(1),
+        location: locations.sample,
+        max_number_of_people: rand(4..16),
+        max_budget_per_month:budgets.sample,
+        group_leader: group_leader
+      )
+    end
+  end
+  
+  def self.create_users_groups
+    SearchingGroup.all.each do |group|
+      UsersGroup.create(
+        user_id: group.group_leader,
+        searching_group_id: group.id
+      )
+    end
+  end
+end
+
+Seed.start
