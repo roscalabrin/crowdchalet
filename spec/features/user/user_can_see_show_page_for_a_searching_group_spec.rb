dd59ef6f8a977066685b55c72cdbb2c351778391
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-17 11:42:20 -0600

    add show page for searching group

--- spec/features/user/user_can_see_show_page_for_a_searching_group_spec.rb
+++ spec/features/user/user_can_see_show_page_for_a_searching_group_spec.rb
@@ -4,7 +4,7 @@ describe "User can see individual page for a searching group" do
   before do
     setup_for_omniauth
   end
-  scenario "User clicks on a group from the group index page and sees all the detailed info about that group" do
+  scenario "User clicks on a group from the group index page and sees all the detailed info of a group they are not a member of" do
     group = create(:searching_group)
     visit root_path
     click_link "Login with Facebook"
@@ -12,8 +12,12 @@ describe "User can see individual page for a searching group" do
     click_link "Denver Couples: Winter 2016"
     
     expect(current_path).to eq(searching_group_path(group))
+    expect(page).to have_content "Denver Couples: Winter 2016"
     expect(page).to have_content "Vail"
-    expect(page).to have_content ""
-    
+    expect(page).to have_content "12"
+    expect(page).to have_content "Max number of people"
+    expect(page).to have_content "4"
+    expect(page).to have_content "Missing"
+    expect(page).to have_button  "Request to Join"
   end
 end
