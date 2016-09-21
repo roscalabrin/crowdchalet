309c86cc3e1f6dded2187d0357c241c6fbc3ed35
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-21 09:36:31 -0600

    add average price to search functionality

--- app/models/house_finder.rb
+++ app/models/house_finder.rb
@@ -10,14 +10,27 @@ class HouseFinder
   end
   
   def return_houses
-    houses = service.search_ski_houses(params)
+    find_houses = service.search_ski_houses(params)
+    avg_price   = average_price(find_houses.second)
+    houses      = house_info(find_houses.first)
+    return  avg_price, houses
+  end
+  
+  def house_info(houses)
     houses.map do |house|
       house_path = house.attributes["href"].value
       HouseListing.new(
           name: house.children.text,
           link: "https://rockies.craigslist.org#{house_path}"
         )
-      end  
+    end 
+  end
+  
+  def average_price(houses)
+    selected_prices = houses.map do |house|
+      house.children.text.split("$").second.to_i
+    end
+    selected_prices.reduce(:+)/selected_prices.count
   end
   
   def service
--- app/services/craigslist_service.rb
+++ app/services/craigslist_service.rb
@@ -3,7 +3,8 @@ class CraigslistService
   def search_ski_houses(params)
     page = HTTParty.get "https://rockies.craigslist.org/search/apa?query=#{params[:location]}&max_price=#{params[:max_price]}"
     parse_page = Nokogiri::HTML(page)
-    parse_page.css('.content').css('.row').css('.hdrlnk').to_a
-    # parse_page.css('.content').css('.row').css('.price').to_a
+    listings = parse_page.css('.content').css('.row').css('.hdrlnk').to_a
+    prices = parse_page.css('.content').css('.row').css('.price').to_a
+    return listings, prices
   end
 end
