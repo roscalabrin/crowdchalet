d0092d3140b0dd72161d599ff45e2071bfe46cb1
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-21 01:07:51 -0600

    edits to searching group show page

--- app/assets/javascripts/listing_likes.js
+++ app/assets/javascripts/listing_likes.js
@@ -3,13 +3,49 @@ $(document).ready(function() {
     var props = {
       initialIsUnliked: $(element).data("initial-is-unliked"),
       userRankingID: $(element).data("user-ranking-id")
-    }
+    } 
     ReactDOM.render(
       React.createElement(LikeListing, props),
       element
     )
   })
+  // $(".location-rank").each(function(index, element) {
+  //   var props = {
+  //     initialIsUnliked: $(element).data("initial-is-ok"),
+  //     userRankingID: $(element).data("user-ranking-id")
+  //   }
+  //   ReactDOM.render(
+  //     React.createElement(LocationRank, props),
+  //     element
+  //   )
+  // })
 })
+
+// var LocationRank = React.createClass({
+//   render: function() {
+//     
+//     var root = <ul className="my-list">
+//              <li>Text Content</li>
+//            </ul>;
+//           //  ReactDOM.render(root, document.getElementById('example'));
+//       //  return React.createElement("select", {onClick: this.handleClick}, "Like");
+//   },
+//   handleClick: function() {
+//     // var method = this.state.isUnliked ? "POST" : "DELETE";
+//     // console.log(this.props.userRankingID);
+//     $.ajax({
+//       url: "/api/v1/likes",
+//       type: "PUT",
+//       data: {listing: this.props.userRankingID},
+//       success: function(response){
+//         this.setState({isUnliked: !response.liked});
+//       }.bind(this),
+//     })
+//   },
+//   getInitialState: function() {
+//     return {isUnliked: this.props.initialIsUnliked};
+//   }
+// })
 
 var LikeListing = React.createClass({
   render: function() {
@@ -35,3 +71,8 @@ var LikeListing = React.createClass({
     return {isUnliked: this.props.initialIsUnliked};
   }
 })
+
+
+  
+  
+// })
